67b2f4f7516f24baaf99486c10f373d54a5e4465
ho-taek <kht072623@naver.com>
2024-04-13 22:52:38 +0900

    빈 조회 방법 -> 이름, 타입

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,36 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("타입으로 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    //구체적으로 적는 것은 좋지 않음.
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
