bbcdbd0d36baac8ff305b01a7a0348768b649cbf
ho-taek <kht072623@naver.com>
2024-07-05 00:44:53 +0900

    파라미터로 받아올 수 있는 것

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -26,7 +26,7 @@ public class MappingClassController {
         return "update userId =" + userId;
     }
 
-    @DeleteMapping("/{userId")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId){
         return "delete userId =" + userId;
     }
